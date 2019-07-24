8bc75d80c878fb60d0be94e194adbb53370c1a63
luckzhu <luckzhu1992@gmail.com>
2019-07-24 15:32:53 +0800

    添加button-group

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,44 @@
 <template>
- <div id="app">
+  <div id="app">
     <div class="mybutton">
-          <zhu-button>默认按钮</zhu-button>
-          <zhu-button type="primary">主要按钮</zhu-button>
-          <zhu-button type="success">成功按钮</zhu-button>
-          <zhu-button type="danger">危险按钮</zhu-button>
-        </div>
-    
-        <div class="mybutton">
-          <zhu-button round>默认按钮</zhu-button>
-          <zhu-button type="primary" round>主要按钮</zhu-button>
-          <zhu-button type="success" round>成功按钮</zhu-button>
-          <zhu-button type="danger" round>危险按钮</zhu-button>
-        </div>
-          <div class="mybutton">
-          <zhu-button round icon="search" >默认按钮</zhu-button>
-          <zhu-button type="primary" round icon="setting" loading iconPosition="right">主要按钮</zhu-button> 
-          <zhu-button type="danger" round icon="delete" loading iconPosition="right">删除按钮</zhu-button> 
-        </div>
- </div>
+      <zhu-button>默认按钮</zhu-button>
+      <zhu-button type="primary">主要按钮</zhu-button>
+      <zhu-button type="success">成功按钮</zhu-button>
+      <zhu-button type="danger">危险按钮</zhu-button>
+    </div>
+
+    <div class="mybutton">
+      <zhu-button round>默认按钮</zhu-button>
+      <zhu-button type="primary" round>主要按钮</zhu-button>
+      <zhu-button type="success" round>成功按钮</zhu-button>
+      <zhu-button type="danger" round>危险按钮</zhu-button>
+    </div>
+    <div class="mybutton">
+      <zhu-button round icon="search">默认按钮</zhu-button>
+      <zhu-button type="primary" round icon="setting" loading iconPosition="right">主要按钮</zhu-button>
+      <zhu-button type="danger" round icon="delete" loading iconPosition="right">删除按钮</zhu-button>
+    </div>
+
+    <div class="mybutton">
+      <zhu-button-group>
+        <zhu-button icon="left">前进</zhu-button>
+        <zhu-button icon="right" iconPosition="right">后退</zhu-button>
+
+      </zhu-button-group>
+    </div>
+  </div>
 </template>
 
 <script>
 import Button from "./components/button.vue";
-import './components/iconfont'
+import "./components/iconfont";
+import ButtonGroup from "./components/button-group.vue";
 
 export default {
   name: "app",
   components: {
-    "zhu-button": Button
+    "zhu-button": Button,
+    "zhu-button-group": ButtonGroup
   }
 };
 </script>
@@ -45,5 +55,4 @@ export default {
   display: inline-flex;
   margin: 20px;
 }
-
 </style>
---
+++ src/components/button-group.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="zhu-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== "button") {
+        console.warn(
+          `zhu-button-group的子元素应该全是 zhu-button,但你写的是${name}`
+        );
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.zhu-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  & > .zhu-button {
+    border-radius: 0;
+    &:first-child {
+      border-top-left-radius: 4px;
+      border-bottom-left-radius: 4px;
+    }
+    &:last-child {
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
+    }
+    &:not(:first-child) {
+      margin-left: -1px;
+    }
+    &:hover {
+      z-index: 1;
+    }
+  }
+}
+</style>
