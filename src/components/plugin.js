fc14eb2b8d5d5e728ab8305a40377740d4197747
luckzhu <luckzhu1992@gmail.com>
2019-08-02 21:33:33 +0800

    实现了toast只能同时存在一个的功能（current套路）

--- src/components/plugin.js
+++ src/components/plugin.js
@@ -1,18 +1,29 @@
 import Toast from './toast.vue'
+
+function createToast({ Vue, message, propsData }) {
+    //生成一个toast组件，然后放到body里面
+    let Constructor = Vue.extend(Toast)
+    let toast = new Constructor({ propsData })
+    //slot要写在$mount之前。default是一个数组。
+    toast.$slots.default = [message]
+    toast.$mount()
+    //注意不要忘了body
+    document.body.appendChild(toast.$el)
+    return toast
+}
+
+let currentToast
+
+
 export default {
-    install(Vue, options){
-        //生成一个toast组件，然后放到body里面
-        Vue.prototype.$toast = function(message,toastOptions){
-            let Constructor = Vue.extend(Toast)
-            let toast = new Constructor({
-                propsData:  toastOptions
-            })
-            //slot要写在$mount之前。default是一个数组。
-            toast.$slots.default = [message]
-            
-            toast.$mount()
-            //注意不要忘了body
-            document.body.appendChild(toast.$el)
+    install(Vue, options) {
+        Vue.prototype.$toast = function (message, toastOptions) {
+            if(currentToast){
+                currentToast.close()
+            }
+            currentToast = createToast({ Vue, message, propsData: toastOptions })
         }
     }
 }
+
+
