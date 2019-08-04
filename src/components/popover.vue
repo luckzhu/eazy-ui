55e05bee09b25bf172ac19ba96725c8c473fbab9
luckzhu <luckzhu1992@gmail.com>
2019-08-04 20:35:43 +0800

    调整代码，不使用.stop阻止冒泡

--- src/components/popover.vue
+++ src/components/popover.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="popover" @click.stop="showContent">
+  <div class="popover" @click="showContent">
     <div
       ref="contentWrapper"
       class="contentWrapper"
       v-if="isPopShow"
-      @click.stop
     >
       <slot name="content"></slot>
     </div>
@@ -22,23 +21,29 @@ export default {
     };
   },
   methods: {
-    showContent() {
-      this.isPopShow = !this.isPopShow;
+    showContent(event) {
+      //只有点中按钮中的元素时才触发showContent
+      if (this.$refs.triggerContent.contains(event.target)) {
+        this.isPopShow = !this.isPopShow;
+      }
+
       if (this.isPopShow === true) {
-        this.$nextTick(() => {
+        setTimeout(() => {
           let { contentWrapper } = this.$refs;
           document.body.appendChild(contentWrapper);
           let { left, top } = this.$refs.triggerContent.getBoundingClientRect();
           //注意是 window.scrollX
           contentWrapper.style.left = left + window.scrollX + "px";
           contentWrapper.style.top = top + window.scrollY + "px";
-          let x = () => {
-            this.isPopShow = false;
-            document.removeEventListener("click", x);
+          let x = e => {
+            if (contentWrapper.contains(e.target)) {
+            } else {
+              this.isPopShow = false;
+              document.removeEventListener("click", x);
+            }
           };
           document.addEventListener("click", x);
-        });
-      } else {
+        }, 0);
       }
     }
   }
@@ -48,11 +53,12 @@ export default {
 <style lang="scss" scoped>
 .popover {
   position: relative;
+  display: inline-block;
 }
 .contentWrapper {
   position: absolute;
   padding: 14px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.65);
-  transform: translateY(-100%)
+  transform: translateY(-100%);
 }
 </style>
