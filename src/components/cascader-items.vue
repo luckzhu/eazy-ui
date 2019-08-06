f78f029f9330c75ba1458e7f6b7739fea360c63e
luckzhu <luckzhu1992@gmail.com>
2019-08-06 19:01:26 +0800

    完成cascader初步显示功能

---
+++ src/components/cascader-items.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="cascader-items">
+    <div class="left">
+      <div
+        class="label"
+        v-for="item in items"
+        :key="item.value"
+        @click="onClickLabel(item)"
+      >
+        {{ item.label }}
+      </div>
+    </div>
+    <div class="right" v-if="rightItems">
+      <ez-cascader-items :items="rightItems"></ez-cascader-items>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EzCascaderItems",
+  data() {
+    return {
+      rightItems: null
+    };
+  },
+  props: {
+    items: {
+      type: Array
+    }
+  },
+  // computed: {
+  //   rightItems(){
+  //     return null
+  //   }
+  // },
+  methods: {
+    onClickLabel(item) {
+      if (item.children) {
+        this.rightItems = item.children;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/cascader.vue
+++ src/components/cascader.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="cascader">
-
+    <div class="trigger" @click="toggle">
+      <slot></slot>
+    </div>
+    <ez-cascader-items :items="source" v-if="isPopVisible"></ez-cascader-items>
   </div>
 </template>
 
 <script>
+import CascaderItems from "./cascader-items";
 export default {
-  name: "EazyCascader"
-}
+  name: "EazyCascader",
+  components: {
+    "ez-cascader-items": CascaderItems
+  },
+  data() {
+    return {
+      isPopVisible: false
+    };
+  },
+  props: {
+    source: {
+      type: Array
+    }
+  },
+  methods: {
+    toggle() {
+      this.isPopVisible = !this.isPopVisible;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  
 </style>
