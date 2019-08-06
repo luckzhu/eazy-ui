c4885398bfb3a6716e3f40981ab87367759241df
luckzhu <luckzhu1992@gmail.com>
2019-08-06 18:07:09 +0800

    初步创建cascader和cascader-item组件

---
+++ src/components/cascader-item.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cascader-item">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EazyCascaderItem"
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
---
+++ src/components/cascader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cascader">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EazyCascader"
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
